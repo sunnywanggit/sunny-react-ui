c9adbc265718756f58ca45915258c62432c2b7bf
ergouwang <330035385@qq.com>
2020-07-20 10:41:17 +0800

    使 icon 组件支持所有事件，外部传入 className 时，不会替换组件原有 className

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 
 1. yarn install
 2. yarn start
+
+## icon 组件
+
+支持所有事件绑定
+
+支持外部传入 className 合并
+
+
+## 项目所用相关插件
+
+- Classnames
+
+A simple JavaScript utility for conditionally joining classNames together.
+
+details and usage:https://www.npmjs.com/package/classnames
+
+
--- lib/icon.tsx
+++ lib/icon.tsx
@@ -2,17 +2,20 @@ import * as React from "react";
 import './icon.scss'
 import './importAllIcons'
 
-interface IconProps{
-    name:String
-    onClick:React.MouseEventHandler<SVGElement >
+interface IconProps extends React.SVGAttributes<SVGElement>{
+    name:string
 }
 
 const Icon:React.FunctionComponent<IconProps>=(props)=>{
+    //为了避免使用者传入的 className 覆盖组件本身的 className
+    //我们先把 className 单独拎出来，然后再和原有的 className 合并
+    const {className,...restProps} = props;
     return(
-        <svg className="sui-icon" onClick={props.onClick}>
+        <svg className={`sui-icon ${className ? className : ''}`}
+             {...restProps} >
             <use xlinkHref={`#${props.name}`}></use>
         </svg>
     )
-}
+};
 
 export default Icon;
