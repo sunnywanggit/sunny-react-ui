3d0d0a2ab012d802d5c69d1ff2fcf7d4d2ae8dee
ergouwang <330035385@qq.com>
2020-07-21 08:20:19 +0800

    配置本地測試文件 example.tsx

---
+++ example.tsx
@@ -0,0 +1,5 @@
+import Icon from "./lib/icon/icon";
+import ReactDom from 'react-dom'
+import React from "react";
+
+ReactDom.render((<div><Icon name="wechat"/></div>),document.querySelector('#root'))
