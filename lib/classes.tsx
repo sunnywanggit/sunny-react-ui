8c909f975db1dfb55e561d613ef53df9d95e481d
ergouwang <330035385@qq.com>
2020-07-26 10:16:35 +0800

    对 dialog 组件添加注释

--- lib/classes.tsx
+++ lib/classes.tsx
@@ -1,3 +1,4 @@
+//使用高阶函数编写帮助函数
 function scopeCalssMaker(prefix:string) {
     return function(name?:string){
         return [prefix,name].filter(Boolean).join('-');
--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,26 +1,26 @@
 import React, {useState} from "react";
 import Dialog from "./dialog";
-import {alert,confirm,model} from './dialog'
+import {alert,confirm,modal} from './dialog'
 
 const DialogExample:React.FunctionComponent=()=>{
-    const [x,setX] = useState(false);
+    const [display,setDisplay] = useState(false);
     return(
         <div>
             <div>
-                <button onClick={()=>{setX(!x)}}>click</button>
-                <Dialog visible={x} closeOnCloseMask={true} buttons={
+                <button onClick={()=>{setDisplay(!display)}}>click</button>
+                <Dialog visible={display} closeOnCloseMask={true} buttons={
                     [
                         <button>1</button>,
                         <button>2</button>
                     ]
-                } onClose={()=>{setX(false)}}>
+                } onClose={()=>{setDisplay(false)}}>
                     <div>hi dialog</div>
                 </Dialog>
             </div>
             <div>
                 <button onClick={()=>{alert('999999')}}>alert</button>
                 <button onClick={()=>{confirm('confirm',()=>{},()=>{})}}>confirm</button>
-                <button onClick={()=>{model(<h1>model</h1>)}}>model</button>
+                <button onClick={()=>{modal(<h1>model</h1>)}}>modal</button>
             </div>
         </div>
     )
