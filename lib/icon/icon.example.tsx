cc53e792cf6771a81787f5ad9acd1b77b738f509
ergouwang <330035385@qq.com>
2020-07-29 17:55:48 +0800

    react-syntax-highlighter 完成代码高亮

--- lib/icon/icon.example.tsx
+++ lib/icon/icon.example.tsx
@@ -1,13 +1,47 @@
-import React from "react";
-import Icon from "./icon";
-
-const IconExample:React.FunctionComponent =()=>{
-    return(
+import React from 'react';
+import Icon from './icon';
+import './icon.example.scss'
+import SyntaxHighlighter from 'react-syntax-highlighter';
+import {github} from 'react-syntax-highlighter/dist/esm/styles/hljs';
+const IconExample: React.FunctionComponent = () => {
+    return (
         <div>
-            <Icon name="wechat"/>
+            <h2>Icon 图标</h2>
+            <p>语义化的矢量图形。使用图标组件，你需要安装 @sunny-react-ui/icons 图标组件包，具体使用方法如下：</p>
+            <SyntaxHighlighter language="javascript" style={github}>
+                {`
+                <Icon name="wechat"/>
+                <Icon name="wechat"/>
+                <Icon name="wechat"/>
+                <Icon name="wechat"/>
+                <Icon name="wechat"/>
+                `}
+
+            </SyntaxHighlighter>
+            <h3>方向性图标</h3>
+            <section className='icon-wrapper'>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+                <span><Icon name="wechat"/></span>
+            </section>
         </div>
-    )
-}
+    );
+};
 
 export default IconExample;
 
