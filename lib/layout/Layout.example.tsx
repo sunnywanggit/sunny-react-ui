c22b5268f9e3cb665314acd74e14398ad9ccbb6a
ergouwang <330035385@qq.com>
2020-08-01 12:58:40 +0800

    Layout 官网页面完成

--- lib/layout/Layout.example.tsx
+++ lib/layout/Layout.example.tsx
@@ -4,46 +4,103 @@ import Content from './Content';
 import Layout from "./layout";
 import Footer from "./footer";
 import Aside from "./aside";
+import './layout.example.scss'
+import SyntaxHighlighter from "react-syntax-highlighter";
+import {github} from "react-syntax-highlighter/dist/esm/styles/hljs";
 
 const LayoutExample:React.FunctionComponent = () => {
     return(
         <div>
-            <h1>第一个例子</h1>
-            <Layout className='hi' style={{height:200,width:800}}>
-                <Header>header</Header>
-                <Content>content</Content>
-                <Footer>footer</Footer>
+            <h2>Layout 布局</h2>
+            <h3>组件概述</h3>
+            <section>
+                <p>Layout：布局容器，其下可嵌套 Header Sider Content Footer 或 Layout 本身，可以放在任何父容器中。</p>
+                <p>Header：顶部布局，自带默认样式，其下可嵌套任何元素，只能放在 Layout 中。</p>
+                <p>Sider：侧边栏，自带默认样式及基本功能，其下可嵌套任何元素，只能放在 Layout 中。</p>
+                <p>Content：内容部分，自带默认样式，其下可嵌套任何元素，只能放在 Layout 中。</p>
+                <p>Footer：底部布局，自带默认样式，其下可嵌套任何元素，只能放在 Layout 中。</p>
+                <p className="grey">注意：采用 flex 布局实现，请注意<span className="notice">浏览器兼容性</span>问题。</p>
+            </section>
+
+            <Layout className='margin30' style={{height:300,width:800}}>
+                <Header className="header">header</Header>
+                <Content className="content">content</Content>
+                <Footer className="footer">footer</Footer>
             </Layout>
 
-            <h1>第二个例子</h1>
-            <Layout className='hi' style={{height:200,width:800}}>
-                <Header>header</Header>
+            <Layout className='margin30 ' style={{height:300,width:800}}>
+                <Header className="header">header</Header>
                 <Layout>
-                    <Aside>aside</Aside>
-                    <Content>content</Content>
+                    <Aside className="aside">aside</Aside>
+                    <Content className="content">content</Content>
                 </Layout>
-                <Footer>footer</Footer>
+                <Footer className="footer">footer</Footer>
             </Layout>
 
-            <h1>第三个例子</h1>
-            <Layout className='hi' style={{height:200,width:800}}>
-                <Header>header</Header>
+            <Layout className='margin30' style={{height:300,width:800}}>
+                <Header className="header">header</Header>
                 <Layout>
-                    <Content>content</Content>
-                    <Aside>aside</Aside>
+                    <Content className="content">content</Content>
+                    <Aside className="aside">aside</Aside>
                 </Layout>
-                <Footer>footer</Footer>
+                <Footer className="footer">footer</Footer>
             </Layout>
 
-            <h1>第四个例子</h1>
-            <Layout>
-                <Aside>aside</Aside>
+            <Layout className="margin30">
+                <Aside className="aside">aside</Aside>
                 <Layout className='hi' style={{height:200,width:800}}>
-                    <Header>header</Header>
-                    <Content>content</Content>
-                    <Footer>footer</Footer>
+                    <Header className="header">header</Header>
+                    <Content className="content">content</Content>
+                    <Footer className="footer">footer</Footer>
                 </Layout>
             </Layout>
+
+
+            <h4>代码展示</h4>
+            <SyntaxHighlighter language="javascript" style={github}>
+
+                {`
+            import {Layout,Header,Aside,Content,Footer } from 'sunny-react-ui';
+            ReactDOM.render(
+                <>
+                    <Layout>
+                        <Header>header</Header>
+                        <Content>content</Content>
+                        <Footer>footer</Footer>
+                    </Layout>
+                    
+                    <Layout>
+                        <Header>header</Header>
+                        <Layout>
+                            <Aside>aside</Aside>
+                            <Content>content</Content>
+                        </Layout>
+                        <Footer>footer</Footer>
+                    </Layout>
+                    
+                    <Layout>
+                        <Header>header</Header>
+                        <Layout>
+                            <Content>content</Content>
+                            <Aside>aside</Aside>
+                        </Layout>
+                        <Footer>footer</Footer>
+                    </Layout>
+                    
+                    <Layout>
+                        <Aside>aside</Aside>
+                        <Layout>
+                            <Header>header</Header>
+                            <Content>content</Content>
+                            <Footer>footer</Footer>
+                        </Layout>
+                    </Layout>
+                </>,
+                mountNode,
+            );
+                `}
+
+            </SyntaxHighlighter>
         </div>
     )
 }
