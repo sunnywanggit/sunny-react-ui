9c0f601c611403a984414228e85949105b8cedda
ergouwang <330035385@qq.com>
2020-08-07 18:03:02 +0800

    完成 Rate 评分组件

--- lib/rate/rate.example.tsx
+++ lib/rate/rate.example.tsx
@@ -1,10 +1,104 @@
 import * as React from 'react';
 import Rate from './rate';
+import SyntaxHighlighter from "react-syntax-highlighter/dist/esm/default-highlight";
+import {atelierPlateauLight} from "react-syntax-highlighter/dist/cjs/styles/hljs";
 
 const RateExample: React.FunctionComponent = () => {
     return (
         <>
-            <Rate/>
+            <h2>Rate 评分组件</h2>
+            <p>评分组件。</p>
+            <h3 className="h3title">何时使用</h3>
+            <section>
+                <p>对评价进行展示</p>
+                <p>对事物进行快速的评级操作。</p>
+            </section>
+            <h3 className="h3title">代码演示</h3>
+            <h4>最简单的用法</h4>
+            <section>
+                <Rate/>
+            </section>
+            <SyntaxHighlighter language="javascript" style={atelierPlateauLight}>
+
+                {`
+    import Rate from 'sunny-react-ui';
+    ReactDOM.render(
+        <>
+        <Rate/>
+        mountNode,
+        </>
+    );
+                `}
+
+            </SyntaxHighlighter>
+            <h4>支持自定义 star 数</h4>
+            <section>
+                <Rate count="3"/>
+            </section>
+            <SyntaxHighlighter language="javascript" style={atelierPlateauLight}>
+
+                {`
+    import Rate from 'sunny-react-ui';
+    ReactDOM.render(
+        <>
+        <Rate count="3"/>
+        mountNode,
+        </>
+    );
+                `}
+
+            </SyntaxHighlighter>
+            <h4>支持默认高亮 star 数</h4>
+            <section>
+                <Rate defaultValue="1"/>
+                <Rate defaultValue="2"/>
+                <Rate defaultValue="3"/>
+                <Rate defaultValue="4"/>
+                <Rate defaultValue="5"/>
+            </section>
+            <SyntaxHighlighter language="javascript" style={atelierPlateauLight}>
+
+                {`
+    import Rate from 'sunny-react-ui';
+    ReactDOM.render(
+        <>
+        <Rate defaultValue="1"/>
+        <Rate defaultValue="2"/>
+        <Rate defaultValue="3"/>
+        <Rate defaultValue="4"/>
+        <Rate defaultValue="5"/>
+        mountNode,
+        </>
+    );
+                `}
+
+            </SyntaxHighlighter>
+            <h4>支持只读，无法进行交互</h4>
+            <section>
+                <Rate disabled/>
+            </section>
+            <SyntaxHighlighter language="javascript" style={atelierPlateauLight}>
+
+                {`
+    import Rate from 'sunny-react-ui';
+    ReactDOM.render(
+        <>
+        <Rate disabled/>
+        mountNode,
+        </>
+    );
+                `}
+
+            </SyntaxHighlighter>
+            <h3 className='h3title'>实现思路概述</h3>
+            <SyntaxHighlighter language="javascript" style={atelierPlateauLight}>
+
+                {`
+    定义两组 star，然后使用绝对定位让"已选中"的 star 去覆盖未选中的 star 
+                `}
+
+            </SyntaxHighlighter>
+
         </>
     );
 
--- lib/rate/rate.tsx
+++ lib/rate/rate.tsx
@@ -1,20 +1,71 @@
 import * as React from 'react';
-import Icon from '../icon/icon';
-import './rate.scss'
+import './rate.scss';
 import scopeClassMaker from '../helpers/classes';
-import {useState} from 'react';
+import {useState, Fragment} from 'react';
+import Icon from '../icon/icon';
+
 const scopedClass = scopeClassMaker('s-rate');
 const sc = scopedClass;
 
-const Rate: React.FunctionComponent = () => {
-    const [voidColor,setVoidColor] = useState('#C6D1DE');
+interface RateProps {
+    //star数
+    count?:string,
+    //默认值
+    defaultValue?:string,
+    //是否允许只读
+    disabled?:Boolean,
+    //失去焦点时的回调
+    // onBlur?:(index:number)=>void
+    //TODO
+    //支持失去焦点/获取焦点/选择时的回调
+}
+
+
+
+const Rate: React.FunctionComponent<RateProps> = (props) => {
+    const [max] = useState(new Array(props.count && +props.count).fill(''));
+    const [width, setWidth] = useState(props.defaultValue && +props.defaultValue*20);
+
+    const decrease = (index: number) => {
+        if(props.disabled) return;
+        setWidth((index ) * 20);
+    };
+    const increase = (index: number) => {
+        if(props.disabled) return;
+        setWidth((index + 1) * 20);
+    };
 
     return (
         <div className={sc()}>
-            <Icon name="star" className={sc('above')}/>
-            <Icon name="star" className={sc('bottom')}/>
+            <div className={sc('icon')}>
+                {max.map((item, index) => {
+                    return (
+                        <Fragment key={index}>
+                            <Icon name="star" className={sc('above')}
+                                  onClick={() => increase(index)}
+                            />
+                        </Fragment>
+                    );
+                })}
+                <div className={sc('icon-active')} style={{width: width}}>
+                    {max.map((item, index) => {
+                        return (
+                            <Icon key={index} name="star" className={sc('bottom')}
+                                  onClick={() => decrease(index)}
+                            />
+                        );
+                    })}
+                </div>
+
+            </div>
         </div>
     );
+};
+
+Rate.defaultProps = {
+    count:'5',
+    defaultValue:'3',
+    disabled:false
 };
 
 export default Rate;
